1d31f1d5ed4a22579e633f4a97b2183836324bd7
Phillip Merriman <phillipmerriman@gmail.com>
2020-10-17 22:13:37 -0500

    added shuffle function to randomize finalPw and added comments to js

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ let sChar = [
 function generatePassword () {
   //create empty array to add random characters to
   let finalPw = [];
-  pw = [];
+  //create empty array for a placeholder for character type arrays
+  let pw = [];
+  //create empty array to make sure specified character types will end up in final password
   let all = [];
 
   //get length of pw
@@ -32,25 +34,27 @@ function generatePassword () {
   }
 
   //get character types that will be in pw, with if/else statements
+  //make variable to confirm whether or not user wants lowercase characters in password
   let lowerCase = confirm("Would you like lowercase characters in your password? \nok = yes, cancel = no.");
-  
+  //if user chose to add lowercase to final password, add lowercase character(s) to password
   if (lowerCase === true) {
+      //add the whole lChar array to pw array
       pw = pw.concat(lChar);
-      // all.push(a random lChar)
+      //add a random lowercase character to the all array to make sure a lowercase will end up in the final pw. - all.push(a random lChar)
       all.push(lChar[Math.floor(Math.random() * lChar.length)]);
-      console.log(all);
+      //alert the user if lowercase characters will be in the password
     alert("Lowercase characters will be in the password.");
   } else {
+    //alert the user if lowercase will NOT be in the password
     alert("Lowercase characters will not be in the password.");
   }
-
+  //repeat for uppercase, numeric, and special characters
   let upperCase = confirm("Would you like uppercase characters in your password? \nok = yes, cancel = no.");
   
   if (upperCase === true) {
       pw = pw.concat(uChar);
       // all.push(a random uChar)
       all.push(uChar[Math.floor(Math.random() * uChar.length)]);
-      console.log(all);
     
     alert("Uppercase characters will be in the password.");
   } else {
@@ -62,7 +66,6 @@ function generatePassword () {
   if (numeric === true) {
     pw = pw.concat(num);
     all.push(num[Math.floor(Math.random() * num.length)]);
-    console.log(all);
     
     alert("Numeric characters will be in the password.");
   } else {
@@ -73,7 +76,6 @@ function generatePassword () {
     alert("Your password will be strictly special characters.");
     pw = pw.concat(sChar);
     all.push(sChar[Math.floor(Math.random() * sChar.length)]);
-    console.log(all);
     
   } else {
     let specialChar = confirm("Would you like special characters in your password? \nok = yes, cancel = no.");
@@ -81,7 +83,6 @@ function generatePassword () {
     if (specialChar === true) {
       pw = pw.concat(sChar);
       all.push(sChar[Math.floor(Math.random() * sChar.length)]);
-      console.log(all);
       
       alert("Special characters will be in the password.");
     } else {
@@ -97,21 +98,30 @@ function generatePassword () {
     let rand = pw[Math.floor(Math.random() * pw.length)];
     finalPw.push(rand);
   }
-
+  
+  //randomize finalPw 
+  shuffle(finalPw);
+  
   //return final pw array, turned into a string
   return finalPw.join("");
 }
-// [l U 0 # ------------]
+//randomize function (Fisher-Yates shuffle: https://bost.ocks.org/mike/shuffle/ )
+function shuffle (array) {
+  for(let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+   
 
 // Write password to the #password input
 function writePassword() {
-  
-
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
